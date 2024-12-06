1038e2c540df989951a552f08fce6f08b8a004b4
Jeanne Lemant <lemant0000@login12.deploy.int>
2024-12-06 18:19:02 +0100

    Code to plot fits by commune

--- 1calibration/03_calibrate.R
+++ 1calibration/03_calibrate.R
@@ -54,7 +54,7 @@ fitdat=read.csv(file.path("../data/PfPR_2-10_sd_Benin_Admin2_2000-2019_MAP_globa
   left_join(good_names) %>%
   mutate_at(vars(prevalenceRate_obs,sd,LCI,UCI,sd_low,sd_high),~./100) %>%
   mutate(age="2-10") %>%
-  select(-c(Admin1,Admin2,source)) %>%
+  select(-c(Admin2,source)) %>%
   mutate(year = as.numeric(year))
 
 #Calibration parameters
@@ -110,3 +110,46 @@ EIRs_calibration <- cali_output$best_EIRs_ci %>%
 
 
 saveRDS(EIRs_calibration,"EIRs_calibration.RDS")
+
+## Plots of calibration by commune
+
+holy_grail = EIRs_calibration %>%
+  select(setting,sub,starts_with("EIR")) %>%
+  pivot_longer(cols=starts_with("EIR"),names_to="EIR_type",values_to="EIR_num") %>%
+  filter(EIR_type %in% c("EIR_lci","EIR","EIR_uci")) %>%
+  mutate(EIR_type=ifelse(EIR_type=="EIR","middle",
+                         ifelse(EIR_type=="EIR_lci","lower","upper")))
+
+past_simuls_calib = all_simul_merged %>%
+  select(setting, year, age, EIR_num, prevalenceRate, Admin1, seed) %>%
+  inner_join(holy_grail,by=c("setting","EIR_num")) %>%
+  select(-EIR_num) %>%
+  pivot_wider(names_from = "EIR_type", values_from = "prevalenceRate", names_prefix = "PR_")
+
+color_map <- rgb(78,176,155,maxColorValue = 255)
+
+for (Department in unique(good_names$Admin1)){
+  calibration_Dpt <- ggplot(past_simuls_calib %>% filter(Admin1 == Department, year <2020))+
+  geom_line(aes(x = year, y = PR_lower, group = seed, color = "C"))+
+  geom_line(aes(x = year, y = PR_upper, group = seed, color = "C"))+
+  geom_line(aes(x = year, y = PR_middle, group = seed, color = "B"))+
+  geom_pointrange(data = fitdat %>% filter(Admin1 == Department, year >= 2006),
+                  aes(x = year, y = prevalenceRate_obs,
+                                     ymin = LCI, ymax = UCI, color = "A"),
+                  size=1,stroke=1,lwd=1,shape=21,fill="white")+
+  labs(y="Malaria prevalence in children aged 2 to 10",x="Year")+
+  scale_color_manual(values = c(color_map,"black","darkgrey"),
+                     name = "",
+                     labels = c("Estimates from MAP used for calibration",
+                                "Simulations from EIR point estimate",
+                                "Simulations from EIR lower and upper bound"))+
+  facet_wrap(sub~.)+
+  theme_minimal(base_size=20)+
+  scale_y_continuous(labels=scales::percent,limits=c(0,1))+
+  theme(legend.position = "bottom")
+  
+  ggsave(calibration_Dpt,filename=paste0(root_dir_path,"fits_calibration/",
+                           "fit_",Department,".png")
+         ,width = 22,height=12
+  )
+}

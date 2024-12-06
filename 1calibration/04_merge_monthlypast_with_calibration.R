7eb9f2ddc226926489cec0dc26dc0ba0db97f051
Jeanne Lemant <lemant0000@login12.deploy.int>
2024-12-06 15:41:31 +0100

    Monthly outputs for calibration reruns

--- 1calibration/04_merge_monthlypast_with_calibration.R
+++ 1calibration/04_merge_monthlypast_with_calibration.R
@@ -1,5 +1,5 @@
 #################################
-# Load past simulations and merge them with calibrated EIRs
+# Extract monthly simulations and merge them with calibrated EIRs
 # 
 # created 26.11.2024 by Jeanne Lemant
 #################################
@@ -8,12 +8,13 @@
 rm(list=ls())
 
 # Load the necessary packages
+library(openMalariaUtilities)
 library(RSQLite)
 library(tidyverse)
 
 # Define root directory with all the experiments according to the user
 if (Sys.getenv("USER") == "lemant0000") {
-  root_dir_path = "/scicore/home/pothin/lemant0000/OpenMalaria/Experiments/BeninSMC/"
+  root_dir_path = "/scicore/home/pothin/lemant0000/OpenMalaria/Experiments/BeninSMCpaper/"
 } else {
   print("Please specify the paths to the necessary folders!")
 }
@@ -21,10 +22,46 @@ if (Sys.getenv("USER") == "lemant0000") {
 # Set working directory to the one where this file is located
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+# Source helper functions
+source("../helper_functions/helper_functions_setup_simulations.R")
+
 expName = "BeninSMC_1calibration"
 
 experiment_folder = paste0(root_dir_path, expName)
+loadExperiment(experiment_folder)
 
+##### This is to set up the extraction of monthly results.
+#If you don't have the omuslurm package installed, just
+# run the batch file  
+# in your terminal and once it has run, go to the next section
+# to read the created database.
+
+## 3. Prepare the scripts for post-processing the OpenMalaria outputs
+# Define the age groups of interest for the outputs (including aggregations)
+age_groups_list = c("0-5","2-10")
+
+# Define the OpenMalaria outputs of interest; these will correspond to the
+# columns of the results table to be stored in the database
+results_columns = c("scenario_id",
+                    "date", "age_group", "date_aggregation",
+                    "nHost", "prevalenceRate")
+
+library(omuslurm)
+# Generate the postprocessing script for monthly results
+# Make sure to adjust the nCPU, memCPU, time and qos if you run larger experiments
+slurmPrepareResults(expDir = getCache("experimentDir"), dbName = paste0(expName,"monthly"),
+                    resultsName = "om_results_monthly", resultsCols = results_columns,
+                    aggrFun = CalcEpiOutputs,
+                    aggrFunArgs = list(indicators = results_columns,
+                                       aggregateByAgeGroup = age_groups_list,
+                                       aggregateByDate = "month"),
+                    ntasks = 1, mem = "50G", nCPU = 30,
+                    strategy = "batch", indexOn = NULL,
+                    rModule = "R/4.2.1-foss-2022a")
+
+slurmRunResults()
+
+##### Start again from here once batch run has been run.
 # extract simulations
 
 # open database connection

7afd6f5e1050d1d23c5cbcecd63904fcde65b403
Jeanne Lemant <jeanne.lemant@swisstph.ch>
2025-01-11 16:33:09 +0100

    Ratio of absolute number of severe cases

--- 3results/3numbers.R
+++ 3results/3numbers.R
@@ -15,9 +15,9 @@ source("./1define_scenarios.R")
 
 targeted_pop_Ext <- futrs %>% filter(year %in% 2024:2026,seed==1,EIR_type=="middle") %>%
   filter((Extension=="Demo" & scenario=="Demo" & age=="5-10" &
-            Admin1 %in% departments_Demoextension)|
+            sub %in% sub_Demoextension)|
            (Extension=="Geo" & scenario=="Geo" & age=="0-5" &
-              Admin1 %in% departments_Geoextension))%>%
+              sub %in% sub_Geoextension))%>%
   group_by(Extension,age,year,scenario) %>%
   summarise(pop=sum(pop)) %>%
   group_by(Extension,age,scenario) %>%
@@ -25,38 +25,38 @@ targeted_pop_Ext <- futrs %>% filter(year %in% 2024:2026,seed==1,EIR_type=="midd
   ungroup() %>%
   rename(target_pop="pop") %>% select(-age)
 
-avertedbaseline_OMcases_Ext <- casesAverted.sumAcrossYears(OMcases_df %>% select(-setting) %>%
+avertedplanned_OMcases_Ext <- casesAverted.sumAcrossYears(OMcases_df %>% select(-setting) %>%
                                                             rename(setting="Extension"),
                                                           cases_col = "OMcases",
                                                           level_aggr = "setting",
                                                           eir_col = "EIR_type",
                                                           EIR_names = c("lower","middle","upper"),
                                                           fut_cols = "scenario",
-                                                          counterfactual = "baseline",
+                                                          counterfactual = "planned",
                                                           year_start = 2024,
                                                           year_end = 2026) %>%
   rename(Extension="setting")
 
-avertedbaseline_nSevere_Ext <- casesAverted.sumAcrossYears(nSevere_df %>% select(-setting) %>%
+avertedplanned_nSevere_Ext <- casesAverted.sumAcrossYears(nSevere_df %>% select(-setting) %>%
                                                             rename(setting="Extension"),
                                                           cases_col = "nSevere",
                                                           level_aggr = "setting",
                                                           eir_col = "EIR_type",
                                                           EIR_names = c("lower","middle","upper"),
                                                           fut_cols = "scenario",
-                                                          counterfactual = "baseline",
+                                                          counterfactual = "planned",
                                                           year_start = 2024,
                                                           year_end = 2026) %>%
   rename(Extension="setting")
 
-avertedbaseline_expectedDirectDeaths_Ext <- casesAverted.sumAcrossYears(expectedDirectDeaths_df %>% select(-setting) %>%
+avertedplanned_expectedDirectDeaths_Ext <- casesAverted.sumAcrossYears(expectedDirectDeaths_df %>% select(-setting) %>%
                                                             rename(setting="Extension"),
                                                           cases_col = "expectedDirectDeaths",
                                                           level_aggr = "setting",
                                                           eir_col = "EIR_type",
                                                           EIR_names = c("lower","middle","upper"),
                                                           fut_cols = "scenario",
-                                                          counterfactual = "baseline",
+                                                          counterfactual = "planned",
                                                           year_start = 2024,
                                                           year_end = 2026) %>%
   rename(Extension="setting")
@@ -65,13 +65,13 @@ avertedbaseline_expectedDirectDeaths_Ext <- casesAverted.sumAcrossYears(expected
 
 targeted_pop_geo_Admin1 <- futrs %>% 
   filter(year %in% 2024:2026,seed==1,EIR_type=="middle",
-         Admin1 %in% departments_Geoextension,scenario=="Geo",age=="0-5")%>%
+         sub %in% sub_Geoextension,scenario=="Geo",age=="0-5")%>%
   group_by(Admin1,age,year,scenario) %>%
   summarise(pop=sum(pop)) %>%
   group_by(Admin1,age,scenario) %>%
   summarise(pop=sum(pop))
 
-avertedbaseline_OMcases_Admin1 <- casesAverted.sumAcrossYears(OMcases_df %>%
+avertedplanned_OMcases_Admin1 <- casesAverted.sumAcrossYears(OMcases_df %>%
                                                                select(-setting) %>%
                                                                mutate(setting=Admin1),
                                                              cases_col = "OMcases",
@@ -79,13 +79,13 @@ avertedbaseline_OMcases_Admin1 <- casesAverted.sumAcrossYears(OMcases_df %>%
                                                              eir_col = "EIR_type",
                                                              EIR_names = c("lower","middle","upper"),
                                                              fut_cols = "scenario",
-                                                             counterfactual = "baseline",
+                                                             counterfactual = "planned",
                                                              year_start = 2024,
                                                              year_end = 2026) %>%
   rename(Admin1="setting") %>%
   filter(scenario=="Geo",Admin1 %in% c("Borgou","Collines","Donga"))
 
-avertedbaseline_nSevere_Admin1 <- casesAverted.sumAcrossYears(nSevere_df %>%
+avertedplanned_nSevere_Admin1 <- casesAverted.sumAcrossYears(nSevere_df %>%
                                                                select(-setting) %>%
                                                                mutate(setting=Admin1),
                                                              cases_col = "nSevere",
@@ -93,13 +93,13 @@ avertedbaseline_nSevere_Admin1 <- casesAverted.sumAcrossYears(nSevere_df %>%
                                                              eir_col = "EIR_type",
                                                              EIR_names = c("lower","middle","upper"),
                                                              fut_cols = "scenario",
-                                                             counterfactual = "baseline",
+                                                             counterfactual = "planned",
                                                              year_start = 2024,
                                                              year_end = 2026) %>%
   rename(Admin1="setting") %>%
   filter(scenario=="Geo",Admin1 %in% c("Borgou","Collines","Donga"))
 
-avertedbaseline_expectedDirectDeaths_Admin1 <- casesAverted.sumAcrossYears(expectedDirectDeaths_df %>%
+avertedplanned_expectedDirectDeaths_Admin1 <- casesAverted.sumAcrossYears(expectedDirectDeaths_df %>%
                                                                select(-setting) %>%
                                                                mutate(setting=Admin1),
                                                              cases_col = "expectedDirectDeaths",
@@ -107,7 +107,7 @@ avertedbaseline_expectedDirectDeaths_Admin1 <- casesAverted.sumAcrossYears(expec
                                                              eir_col = "EIR_type",
                                                              EIR_names = c("lower","middle","upper"),
                                                              fut_cols = "scenario",
-                                                             counterfactual = "baseline",
+                                                             counterfactual = "planned",
                                                              year_start = 2024,
                                                              year_end = 2026) %>%
   rename(Admin1="setting") %>%
@@ -116,90 +116,96 @@ avertedbaseline_expectedDirectDeaths_Admin1 <- casesAverted.sumAcrossYears(expec
 ##### Geographic versus demographic extension
 
 #cases averted
-avertedbaseline_OMcases_Ext %>% filter(Extension=="Demo"&scenario=="Demo")
+avertedplanned_OMcases_Ext %>% filter(Extension=="Demo"&scenario=="Demo")
 
-avertedbaseline_nSevere_Ext %>% filter(Extension=="Demo"&scenario=="Demo")
+avertedplanned_nSevere_Ext %>% filter(Extension=="Demo"&scenario=="Demo")
 
-avertedbaseline_expectedDirectDeaths_Ext %>% filter(Extension=="Demo"&scenario=="Demo")
+avertedplanned_expectedDirectDeaths_Ext %>% filter(Extension=="Demo"&scenario=="Demo")
 
-avertedbaseline_OMcases_Ext %>% filter(Extension=="Geo"&scenario=="Geo")
+avertedplanned_OMcases_Ext %>% filter(Extension=="Geo"&scenario=="Geo")
 
-avertedbaseline_nSevere_Ext %>% filter(Extension=="Geo"&scenario=="Geo")
+avertedplanned_nSevere_Ext %>% filter(Extension=="Geo"&scenario=="Geo")
 
-avertedbaseline_expectedDirectDeaths_Ext %>% filter(Extension=="Geo"&scenario=="Geo")
+avertedplanned_expectedDirectDeaths_Ext %>% filter(Extension=="Geo"&scenario=="Geo")
 
 #eligible population
 targeted_pop_Ext %>% select(Extension,target_pop)
 
 #division of cases averted by targeted population
-right_join(avertedbaseline_OMcases_Ext,targeted_pop_Ext) %>%
+right_join(avertedplanned_OMcases_Ext,targeted_pop_Ext) %>%
   mutate(across(starts_with("ca"),~./target_pop*1000)) %>%
   filter(Extension=="Demo")
 
-right_join(avertedbaseline_nSevere_Ext,targeted_pop_Ext) %>%
+right_join(avertedplanned_nSevere_Ext,targeted_pop_Ext) %>%
   mutate(across(starts_with("ca"),~./target_pop*1000)) %>%
   filter(Extension=="Demo")
 
-right_join(avertedbaseline_expectedDirectDeaths_Ext,targeted_pop_Ext) %>%
+right_join(avertedplanned_expectedDirectDeaths_Ext,targeted_pop_Ext) %>%
   mutate(across(starts_with("ca"),~./target_pop*10^6)) %>%
   filter(Extension=="Demo")
 
-right_join(avertedbaseline_OMcases_Ext,targeted_pop_Ext) %>%
+right_join(avertedplanned_OMcases_Ext,targeted_pop_Ext) %>%
   mutate(across(starts_with("ca"),~./target_pop*1000)) %>%
   filter(Extension=="Geo")
 
-right_join(avertedbaseline_nSevere_Ext,targeted_pop_Ext) %>%
+right_join(avertedplanned_nSevere_Ext,targeted_pop_Ext) %>%
   mutate(across(starts_with("ca"),~./target_pop*1000)) %>%
   filter(Extension=="Geo")
 
-right_join(avertedbaseline_expectedDirectDeaths_Ext,targeted_pop_Ext) %>%
+right_join(avertedplanned_expectedDirectDeaths_Ext,targeted_pop_Ext) %>%
   mutate(across(starts_with("ca"),~./target_pop*10^6)) %>%
   filter(Extension=="Geo")
 
 #ratio
-right_join(avertedbaseline_OMcases_Ext,targeted_pop_Ext) %>%
+right_join(avertedplanned_OMcases_Ext,targeted_pop_Ext) %>%
   mutate(across(starts_with("ca"),~./target_pop*1000)) %>%
   filter(Extension=="Geo") %>% pull(ca.tot.mean) /
-  right_join(avertedbaseline_OMcases_Ext,targeted_pop_Ext) %>%
+  right_join(avertedplanned_OMcases_Ext,targeted_pop_Ext) %>%
   mutate(across(starts_with("ca"),~./target_pop*1000)) %>%
   filter(Extension=="Demo") %>% pull(ca.tot.mean)
 
-right_join(avertedbaseline_nSevere_Ext,targeted_pop_Ext) %>%
+right_join(avertedplanned_nSevere_Ext,targeted_pop_Ext) %>%
   mutate(across(starts_with("ca"),~./target_pop*1000)) %>%
   filter(Extension=="Geo") %>% pull(ca.tot.mean) /
-  right_join(avertedbaseline_nSevere_Ext,targeted_pop_Ext) %>%
+  right_join(avertedplanned_nSevere_Ext,targeted_pop_Ext) %>%
   mutate(across(starts_with("ca"),~./target_pop*1000)) %>%
   filter(Extension=="Demo") %>% pull(ca.tot.mean)
 
-right_join(avertedbaseline_expectedDirectDeaths_Ext,targeted_pop_Ext) %>%
+right_join(avertedplanned_expectedDirectDeaths_Ext,targeted_pop_Ext) %>%
   mutate(across(starts_with("ca"),~./target_pop*10^6)) %>%
   filter(Extension=="Geo") %>% pull(ca.tot.mean) /
-  right_join(avertedbaseline_expectedDirectDeaths_Ext,targeted_pop_Ext) %>%
+  right_join(avertedplanned_expectedDirectDeaths_Ext,targeted_pop_Ext) %>%
   mutate(across(starts_with("ca"),~./target_pop*10^6)) %>%
   filter(Extension=="Demo") %>% pull(ca.tot.mean)
+
+avertedplanned_nSevere_Ext %>% filter(Extension=="Geo"&scenario=="Geo") %>%
+  pull(ca.tot.mean) /
+  avertedplanned_nSevere_Ext %>% filter(Extension=="Demo"&scenario=="Demo") %>% 
+  pull(ca.tot.mean)
+
 
 ##### Prioritisation among geographic extension
-right_join(avertedbaseline_OMcases_Admin1 %>% ungroup() %>% select(-age),
+right_join(avertedplanned_OMcases_Admin1 %>% ungroup() %>% select(-age),
            targeted_pop_geo_Admin1) %>%
   mutate(across(starts_with("ca"),~./pop*1000)) %>% filter(Admin1=="Borgou")
 
-right_join(avertedbaseline_nSevere_Admin1 %>% ungroup() %>% select(-age),
+right_join(avertedplanned_nSevere_Admin1 %>% ungroup() %>% select(-age),
            targeted_pop_geo_Admin1) %>%
   mutate(across(starts_with("ca"),~./pop*1000)) %>% filter(Admin1=="Borgou")
 
-right_join(avertedbaseline_expectedDirectDeaths_Admin1 %>% ungroup() %>% select(-age),
+right_join(avertedplanned_expectedDirectDeaths_Admin1 %>% ungroup() %>% select(-age),
            targeted_pop_geo_Admin1) %>%
   mutate(across(starts_with("ca"),~./pop*10^6)) %>% filter(Admin1=="Borgou")
 
-right_join(avertedbaseline_OMcases_Admin1 %>% ungroup() %>% select(-age),
+right_join(avertedplanned_OMcases_Admin1 %>% ungroup() %>% select(-age),
            targeted_pop_geo_Admin1) %>%
   mutate(across(starts_with("ca"),~./pop*1000)) %>% filter(Admin1=="Donga")
 
-right_join(avertedbaseline_nSevere_Admin1 %>% ungroup() %>% select(-age),
+right_join(avertedplanned_nSevere_Admin1 %>% ungroup() %>% select(-age),
            targeted_pop_geo_Admin1) %>%
   mutate(across(starts_with("ca"),~./pop*1000)) %>% filter(Admin1=="Donga")
 
-right_join(avertedbaseline_expectedDirectDeaths_Admin1 %>% ungroup() %>% select(-age),
+right_join(avertedplanned_expectedDirectDeaths_Admin1 %>% ungroup() %>% select(-age),
            targeted_pop_geo_Admin1) %>%
   mutate(across(starts_with("ca"),~./pop*10^6)) %>% filter(Admin1=="Donga")
 
